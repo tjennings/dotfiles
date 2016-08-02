9a6c17478f33dccebf484d885f1d4360180cc6ed
Tyler Jennings <tyle.jennings@gmail.com>
2016-08-02 11:49:54 -0500

    moving to prelude's evil setup

--- prelude_personal/custom.el
+++ prelude_personal/custom.el
@@ -1,6 +1,5 @@
-(prelude-require-packages '(evil evil-tabs))
+(prelude-require-packages '(evil-tabs))
 
-(require 'evil)
 (evil-mode 1)
 
 (menu-bar-mode -1)
@@ -19,7 +18,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-tabs evil zop-to-char zenburn-theme yari which-key web-mode volatile-highlights vkill undo-tree smartrep smartparens smart-mode-line ruby-tools rainbow-mode rainbow-delimiters projectile ov operate-on-number move-text magit json-mode js2-mode inf-ruby imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck expand-region exec-path-from-shell ensime elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu anaconda-mode ace-window))))
+    (markdown-mode evil-tabs evil zop-to-char zenburn-theme yari which-key web-mode volatile-highlights vkill undo-tree smartrep smartparens smart-mode-line ruby-tools rainbow-mode rainbow-delimiters projectile ov operate-on-number move-text magit json-mode js2-mode inf-ruby imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck expand-region exec-path-from-shell ensime elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu anaconda-mode ace-window))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
