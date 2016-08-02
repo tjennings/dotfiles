fdb26a22dd8015e6aa76d9a3a30875d09d1f4a3a
Tyler Jennings <ltj@Tylers-MacBook.local>
2016-08-02 11:09:06 -0500

    prelude personal files

---
+++ prelude_personal/custom.el
@@ -0,0 +1,29 @@
+(prelude-require-packages '(evil evil-tabs))
+
+(require 'evil)
+(evil-mode 1)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(global-evil-tabs-mode t)
+
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (evil-tabs evil zop-to-char zenburn-theme yari which-key web-mode volatile-highlights vkill undo-tree smartrep smartparens smart-mode-line ruby-tools rainbow-mode rainbow-delimiters projectile ov operate-on-number move-text magit json-mode js2-mode inf-ruby imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck expand-region exec-path-from-shell ensime elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux browse-kill-ring beacon anzu anaconda-mode ace-window))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
